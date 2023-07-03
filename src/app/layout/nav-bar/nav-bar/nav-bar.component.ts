3335896de24d924e64865258084b9e93cf2ad188
LAMIS USAMA <lamis.usama@smarttechsys.com>
2023-07-03 18:22:54 +0300

    logout

--- src/app/layout/nav-bar/nav-bar/nav-bar.component.ts
+++ src/app/layout/nav-bar/nav-bar/nav-bar.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { UserService } from 'src/app/modules/users/services/user.service';
+import { UserLoggedIn } from 'src/app/sharedFeatures/models/user-login.model';
+import { CurrentUserService } from 'src/app/sharedFeatures/services/current-user.service';
+import { LanguageService } from 'src/app/sharedFeatures/services/language.service';
+import { NotificationService } from 'src/app/sharedFeatures/services/notification.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+  currentUser: UserLoggedIn | null = null;
+  domainUrl: string = this._UsersService.domainUrl;
 
+  constructor(
+    private currentUserService: CurrentUserService,
+    private router: Router,
+    private notificationService: NotificationService,
+
+    private _UsersService: UserService,
+    private translateService: TranslateService,
+    public languageService: LanguageService
+  ) {}
+
+  ngOnInit(): void {
+    this.currentUser = this.currentUserService.getCurrentUser();
+
+    if (this.currentUser == null) {
+      this.router.navigate(['/login']);
+    }
+  }
+  userLogout() {
+    this.currentUserService.logOut();
+    this.router.navigate(['/login']);
+  }
 }
--- src/app/modules/users/services/user.service.ts
+++ src/app/modules/users/services/user.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateService } from '@ngx-translate/core';
+import { BaseHttpServiceService } from 'src/app/sharedFeatures/services/base-http-service.service';
+import { CurrentUserService } from 'src/app/sharedFeatures/services/current-user.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class UserService {
-
-  constructor() { }
+export class UserService extends BaseHttpServiceService {
+  // private controller: string = `${this.baseUrl}/Users/`;
+  constructor(
+    http: HttpClient,
+    translateService: TranslateService,
+    currentUserService: CurrentUserService
+  ) {
+    super(http, translateService, currentUserService);
+  }
 }
